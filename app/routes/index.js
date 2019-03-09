ede953f1be059bcc2d285ce4f348355a6a293146
Bellisa <olesya.kurilenko@gmail.com>
2019-03-10 01:00:31 +0200

    first commit

---
+++ app/routes/index.js
@@ -0,0 +1,11 @@
+const users = require('./users');
+const products = require('./products');
+const categories = require('./catagories');
+const favorite = require('./favorite');
+
+module.exports = {
+  users,
+  products,
+  categories,
+  favorite
+};
